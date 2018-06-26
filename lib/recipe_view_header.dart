4596e638c04de5501e581fc3aeeb65ce3e263543
Treetender <adam.acott@gmail.com>
2018-06-26 03:35:38 +0000

    tweaked margins for header

--- lib/recipe_view_header.dart
+++ lib/recipe_view_header.dart
@@ -11,43 +11,40 @@ class RecipeHeader extends StatelessWidget {
     final headLineTheme =
         Theme.of(context).textTheme.headline.apply(color: Colors.white);
 
-    return SizedBox(
-      height: 450.0,
-          child: Stack(
-        fit: StackFit.expand,
-        children: [
-        AspectRatio(
-          aspectRatio: 16 / 9,
-          child: Container(
-            margin: const EdgeInsets.only(bottom: 32.0),
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              image: DecorationImage(
-                  image: recipe.recipeImage,
-                  alignment: FractionalOffset.center,
-                  fit: BoxFit.fitWidth),
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+      AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 32.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            image: DecorationImage(
+                image: recipe.recipeImage,
+                alignment: FractionalOffset.center,
+                fit: BoxFit.fitWidth),
 
-            ),
           ),
         ),
-        Positioned(
+      ),
+      Positioned(
+        child: Text(
+          recipe.name,
+          textAlign: TextAlign.start,
+          style: headLineTheme,
+          softWrap: true,
+        ),
+        bottom: 32.0,
+        left: 16.0,
+      ),
+      Positioned(
           child: Text(
-            recipe.name,
-            textAlign: TextAlign.start,
-            style: headLineTheme,
-            softWrap: true,
+            "Time: 1 hour",
+            style: TextStyle(color: Colors.white),
           ),
           bottom: 32.0,
-          left: 16.0,
-        ),
-        Positioned(
-            child: Text(
-              "Time: 1 hour",
-              style: TextStyle(color: Colors.white),
-            ),
-            bottom: 32.0,
-            right: 16.0),
-      ]),
-    );
+          right: 16.0),
+    ]);
   }
 }
